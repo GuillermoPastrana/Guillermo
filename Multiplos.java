4d406a8602c812be34e2919c26f83ef8dec80b47
GuillermoPastrana <57964574+GuillermoPastrana@users.noreply.github.com>
2019-12-04 00:30:56 -0600

    Add files via upload

---
+++ Multiplos.java
@@ -0,0 +1,23 @@
+package multiplos;
+
+import java.util.Scanner;
+public class Multiplos {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int SumaTotal = 0;
+        
+        System.out.println("Introduce el numero:");
+        int Numero = scan.nextInt();
+       
+        for (int i = 1; i < Numero; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                SumaTotal = i + SumaTotal;
+            }
+        }
+
+        System.out.println(SumaTotal);
+    }
+
+}
+
