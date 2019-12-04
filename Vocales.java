de31389c01e7e2a0a0a1da3be2fb35725444cc09
GuillermoPastrana <57964574+GuillermoPastrana@users.noreply.github.com>
2019-12-04 00:11:07 -0600

    Add files via upload

---
+++ Vocales.java
@@ -0,0 +1,22 @@
+package vocales;
+
+import java.util.Scanner;
+
+public class Vocales {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String Palabra;
+        int Lector = 0;
+
+        System.out.println("Introduce una palabra");
+        Palabra = scan.nextLine();
+        for (int i = 0; i < Palabra.length(); i++) {
+            if ((Palabra.charAt(i) == 'a') || (Palabra.charAt(i) == 'e') || (Palabra.charAt(i) == 'i')
+                    || (Palabra.charAt(i) == 'o') || (Palabra.charAt(i) == 'u')) {
+                Lector++;
+            }
+        }
+        System.out.println("La palabra " + Palabra + " contiene " + Lector + " vocales");
+    }
+}
